28f50fb97bb8b3f114c092074154e2e66345bcf4
StephHarrison-Wood <67841330+StephHarrison-Wood@users.noreply.github.com>
2021-01-25 13:59:34 +0000

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://laughing-chandrasekhar-438be0.netlify.app/"
-          target="_blank"
-        >
-          Stephanie Harrison-Wood
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/StephHarrison-Wood/Steph-Weather-App"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://laughing-chandrasekhar-438be0.netlify.app/"
+            target="_blank"
+          >
+            Stephanie Harrison-Wood
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/StephHarrison-Wood/Steph-Weather-App"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
