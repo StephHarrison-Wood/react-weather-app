6a17ca188cbfed23ca1865b2bbd395695a2c7f3c
StephHarrison-Wood <67841330+StephHarrison-Wood@users.noreply.github.com>
2021-02-02 14:51:30 +0000

    Added weather future weather forcast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,6 +19,7 @@ export default function WeatherInfo(props) {
             <div className="float-left">
               <WeatherIcon code={props.data.icon} />
             </div>
+
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
